af1a887eca920a052c864f6b45776f68f02f176c
Veronicah-155 <njugunaveraiconicah.gmail.com>
2023-07-13 01:18:40 +0000

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+0-lookup module
+"""
+
+
+def lookup(obj):
+    """
+    function that retrieves the available attributes
+    and methods of an object using the dir() function
+    """
+    return dir(obj)
