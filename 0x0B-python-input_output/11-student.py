a267eb0cf78c5ae33a972155cb5cb93cf557ba76
Veronicah-155 <njugunaveraiconicah.gmail.com>
2023-07-13 00:47:28 +0000

    Task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" creating student class """
+
+
+class Student:
+    """ Define the class"""
+    def __init__(self, first_name, last_name, age):
+        """ Initializing a class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieve the dictionary representation """
+        if attrs is None or not isinstance(attrs, list):
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json_data):
+        """  replaces all attributes of the Student instance """
+        for key, value in json_data.items():
+            setattr(self, key, value)
